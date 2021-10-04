c539ba4988b0b5c4d0ed7b1d9f33b2aa8f30cc8f
Adam Rizk <adamrizk9@gmail.com>
2021-10-04 18:01:39 +0530

    Added Set

---
+++ STL/set.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <iterator>
+#include <list>
+using namespace std;
+
+void printArray(int arr[], int n){
+    for(int i=0;i<n;++i) cout<<arr[i]<<" ";
+    cout<<endl;
+}
+
+void printSet(set<int> testSet){
+  for(auto itr=testSet.begin();itr!=testSet.end();++itr)
+    cout<<*itr<<" ";
+  cout<<endl;
+}
+
+
+int main(int argc, char **argv){
+  //lists are STL implementation of linked list, therefore, it does not have random access
+  //But elements can be inserted or deleted at any point efficiently, since memory is not contiguous
+  if(argc<2) {cout<<"Provide the input file as a command line argument"; return 0;}
+  ifstream is(argv[1]);
+  int cnt=0;
+  int temp=0;
+  is>>cnt;
+  set<int> testSet; //Declaring an STL set. Obviously, we do not specify the length.
+  for(int i=0;i<cnt;++i){
+    is>>temp;
+    testSet.insert(temp); //Adding elements to the end of the list.
+  }
+  is.close();
+  printSet(testSet); //Notice how the set automatically stores values in sorted order
+  testSet.insert(11); //Doesn't get inserted since 11 is already present
+  testSet.erase(11); //Elements cannot be modified, only inserted and deleted
+  if(testSet.find(11)==testSet.end()) cout<<"testSet doesn't contain value 11"<<endl;
+  
+  //Overall, set and multiset are very similar to map and multimap respectively, except the keys exist on their own instead
+  //of being mapped to another value
+
+  return 0;
+} 
