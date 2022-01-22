dead53f0447f6845a91a04d9c5eeaba4df8c53f9
Adam Rizk <adamrizk9@gmail.com>
2022-01-22 16:42:35 +0530

    Added Java boilerplate code

--- DSA/twoSums/TwoSums.java
+++ DSA/twoSums/TwoSums.java
@@ -1,24 +1,25 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.*;
 
 public class TwoSums {
 
     public static void main(String[] args) {
-        System.out.println(new File(".").getAbsoluteFile());
         String fileName = "/home/adam/Documents/Practice/DSA/twoSums/input.txt";
         File file = new File(fileName);
-	System.out.println(file.canRead());
-	try{
-	FileReader fr = new FileReader(file);
-	BufferedReader br = new BufferedReader(fr);
-	String line;
-	while((line = br.readLine()) != null){
-    		//process the line
-    		System.out.println(line);
-	}
-	}
-	catch(Exception e){
-		System.out.println("We had an exception");
-	}
+        int temp;
+        List<Integer> arr = new ArrayList<Integer>();
+		try{
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			while((line = br.readLine()) != null){
+    			arr.add(Integer.parseInt(line));
+			}
+		} catch(Exception e){
+			System.out.println("We had an exception");
+		}
+		
+		System.out.println(arr);
     }
 }
