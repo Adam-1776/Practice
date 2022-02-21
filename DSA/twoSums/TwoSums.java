eea7f7ccd571beea91c16fc5bdee82e52f36be1f
Adam Rizk <adamrizk9@gmail.com>
2022-02-22 04:31:36 +0530

    cleaned up file reading

--- DSA/twoSums/TwoSums.java
+++ DSA/twoSums/TwoSums.java
@@ -4,22 +4,24 @@ import java.util.*;
 
 public class TwoSums {
 
-    public static void main(String[] args) {
-        String fileName = "/home/adam/Documents/Practice/DSA/twoSums/input.txt";
-        File file = new File(fileName);
-        int temp;
-        List<Integer> arr = new ArrayList<Integer>();
+	public static void readFile(List arr, String fileName) {
+		File file = new File(fileName);
 		try{
 			FileReader fr = new FileReader(file);
 			BufferedReader br = new BufferedReader(fr);
-			String line;
+			String line = "";
 			while((line = br.readLine()) != null){
     			arr.add(Integer.parseInt(line));
 			}
 		} catch(Exception e){
-			System.out.println("We had an exception");
+			System.out.println(e);
 		}
-		
+	}
+
+    public static void main(String[] args) {
+        String fileName = "D:\\Documents\\Practice\\DSA\\twoSums\\input.txt";
+        List<Integer> arr = new ArrayList(100);
+		readFile(arr, fileName);
 		System.out.println(arr);
     }
 }
