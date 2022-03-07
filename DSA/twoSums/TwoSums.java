7cc536f3045c3d2b5a40c5448dd9c45d5e5b5202
Adam Rizk <adamrizk9@gmail.com>
2022-03-07 18:34:32 +0530

    Changed import statements

--- DSA/twoSums/TwoSums.java
+++ DSA/twoSums/TwoSums.java
@@ -1,5 +1,11 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 //https://leetcode.com/problems/two-sum
 
