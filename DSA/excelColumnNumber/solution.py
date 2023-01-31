8a608dfe7a1a41c9f0bc8409b1034ab6900c7393
Adam Rizk <adamrizk9@gmail.com>
2023-01-31 22:14:48 +0530

    Added Excel Column Number

---
+++ DSA/excelColumnNumber/solution.py
@@ -0,0 +1,19 @@
+#https://leetcode.com/problems/excel-sheet-column-number/
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        column = 0 #Base 10 representation of columnTitle
+        digit = 0 #Which place in the string we're looking at, starting from the right and with zero indexing
+        for c in reversed(columnTitle) :
+            column += (ord(c) - ord('A') + 1) * (26 ** digit) #Formula to find the base10 'value' of a digit in a base26 number
+            digit += 1
+        return column
+
+#Basically we treat columnTitle like a base26 number and convert it to base10
+
+def main():
+    solution = Solution()
+    print(solution.titleToNumber("BCA")) # 1431
+
+if __name__ == "__main__": #Entry point
+    main() #Calling main method
