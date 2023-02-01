7ce54339acb5b6ba64b0e89a76560ce2ba4ed392
Adam Rizk <adamrizk9@gmail.com>
2023-02-01 06:28:12 +0530

    Added power of three

---
+++ DSA/powerOfThree/solution.py
@@ -0,0 +1,23 @@
+from math import log10
+from math import log
+from math import isclose
+
+#https://leetcode.com/problems/power-of-three/
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0 : return False
+        return (log10(n) / log10(3)).is_integer()
+
+    def isPowerOfThree2(self, n: int) -> bool:
+        logVal = log(n,3) #Probably has strange floating point behavior
+        return isclose(logVal,round(logVal)) #If logVal is very close to its rounded integer, then logVal should have been an integer
+
+
+def main():
+    solution = Solution()
+    print(solution.isPowerOfThree(243))
+    print(solution.isPowerOfThree2(243))
+
+if __name__ == "__main__": #Entry point
+    main() #Calling main method
