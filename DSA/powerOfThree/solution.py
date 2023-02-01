9c389e5f329d1083a0b04f925c9efde1beecf9b5
Adam Rizk <adamrizk9@gmail.com>
2023-02-01 06:28:43 +0530

    Added power of three

--- DSA/powerOfThree/solution.py
+++ DSA/powerOfThree/solution.py
@@ -9,6 +9,7 @@ class Solution:
         if n <= 0 : return False
         return (log10(n) / log10(3)).is_integer()
 
+    #Alternate method using math.isclose()
     def isPowerOfThree2(self, n: int) -> bool:
         logVal = log(n,3) #Probably has strange floating point behavior
         return isclose(logVal,round(logVal)) #If logVal is very close to its rounded integer, then logVal should have been an integer
