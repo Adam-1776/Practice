cf6a01f47d2a52ed36234d878d93689cf5351ff5
Adam Rizk <adamrizk9@gmail.com>
2023-02-01 06:50:57 +0530

    missingNumber

--- DSA/missingNumber/solution.py
+++ DSA/missingNumber/solution.py
@@ -1,12 +1,15 @@
 #https://leetcode.com/problems/missing-number/
 
 class Solution:
+    #Linear time complexity, linear space complexity
     def missingNumber(self, nums: list[int]) -> int:
         #We know that numbers from 0 upto and including len(nums) can be in the list. 
         numSet = set(range(0,len(nums)+1)) #We add one to the second paramter to make sure len(nums) is included
         for n in nums:
             numSet.discard(n)
         return numSet.pop() #The only digit left is the answer
+
+    #To-do: implement a O(1) space complexity solution using bit manipulation or  Gauss' Formula
 
 
 def main():
