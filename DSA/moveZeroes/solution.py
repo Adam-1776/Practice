f4d8da81f603ef8ccdb6e88b006a4b2bd6b7c19e
Adam Rizk <adamrizk9@gmail.com>
2023-02-07 13:37:30 +0530

    Added moveZeroes problem

---
+++ DSA/moveZeroes/solution.py
@@ -0,0 +1,33 @@
+#https://leetcode.com/problems/move-zeroes/
+#This problem is closely related to the 'remove element' leetcode problem
+
+class Solution:
+    def moveZeroes(self, nums: list) -> None:
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0 and nums[slow] == 0: #If we find a 0 to the left of our right pointer, swap them
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+
+            if nums[slow] != 0: #Increment the left pointer if it is not pointing to a zero
+                slow += 1
+
+    def moveZeroes2(self, nums: list) -> None:
+        left = 0
+        right = 0
+        for right in range(len(nums)):
+            nums[left] = nums[right]
+            if nums[left] != 0:
+                left += 1
+        #After the loop above, we have removed all zeroes and preserved order till index left. Now fill zeroes in the remaining indexes
+        for i in range(left,len(nums)):
+            nums[i] = 0
+            
+
+def main():
+    solution = Solution()
+    list1 = [21,0,0,21,0,37,25,21]
+    print(solution.moveZeroes(list1))
+    print(list1)
+
+if __name__ == "__main__": #Entry point
+    main() #Calling main method
--- DSA/removeElement/solution.py
+++ DSA/removeElement/solution.py
@@ -1,7 +1,7 @@
 #https://leetcode.com/problems/remove-element/
 
 class Solution:
-    #We use a two pointer approach to delete the unwatned value in-place
+    #We use a two pointer approach to delete the unwanted value in-place. It preserves the relative order of the elements
     def removeElement(self, nums: list[int], val: int) -> int:
         l = 0
         r = 0 #Initialize left and right pointers
@@ -13,6 +13,20 @@ class Solution:
                 l += 1 #If we copy a non-unwanted value, increment the left pointer. Else the left pointer does not get incremented
             r += 1 #We increment the right pointer every time
         return l #Return length of new list
+
+    #Alternative solution. It moves the unwanted values to the right of the list.
+    def removeElement2(self, nums: list[int], val: int) -> int:
+        l = 0
+        r = 0 #Initialize left and right pointers
+        while r < len(nums):
+            #print(nums)
+            if nums[l] == val and nums[r] != val: #If left is unwanted and right is not unwanted, swap them. This moves the unwanted value towards the right
+                nums[l],nums[r] = nums[r],nums[l]
+            if nums[l] != val :
+                l += 1
+            r += 1
+        return l
+            
 
 #The approach above us to use one pointer to keep track of which index to copy the next value to. If we copy the unwanted value
 #Then we do not increment the left pointer which will cause the unwanted value there to get overwritten.
