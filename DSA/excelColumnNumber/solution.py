5a9920c24cd4c054bf7d2026aa415c415e6dda6c
Adam Rizk <adamrizk9@gmail.com>
2023-02-17 22:55:32 +0530

    Added excel problem

--- DSA/excelColumnNumber/solution.py
+++ DSA/excelColumnNumber/solution.py
@@ -1,6 +1,8 @@
 #https://leetcode.com/problems/excel-sheet-column-number/
+#https://leetcode.com/problems/excel-sheet-column-title/
 
 class Solution:
+    #Basically we treat columnTitle like a base26 number and convert it to base10
     def titleToNumber(self, columnTitle: str) -> int:
         column = 0 #Base 10 representation of columnTitle
         digit = 0 #Which place in the string we're looking at, starting from the right and with zero indexing
@@ -10,11 +12,19 @@ class Solution:
             digit += 1
         return column
 
-#Basically we treat columnTitle like a base26 number and convert it to base10
+    def convertToTitle(self, columnNumber: int) -> str:
+        columnTitle = ""
+        while columnNumber > 0 :
+            columnNumber -= 1 #Decrement by one, since the first alphabet 'A' represents 1, not 0
+            letter = columnNumber % 26
+            columnTitle += chr(letter + ord('A'))
+            columnNumber = columnNumber // 26
+        return columnTitle[::-1] 
 
 def main():
     solution = Solution()
     print(solution.titleToNumber("BCA")) # 1431
+    print(solution.convertToTitle(1431)) # BCA
 
 if __name__ == "__main__": #Entry point
     main() #Calling main method
