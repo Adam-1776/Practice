cfa7b1b88d2fa351d89db5b4a6665586c8b44164
Adam Rizk <adamrizk9@gmail.com>
2023-02-18 20:09:44 +0530

    isometric strings

--- DSA/isomorphicStrings/solution.py
+++ DSA/isomorphicStrings/solution.py
@@ -3,7 +3,7 @@
 class Solution:
     #The key to this problem is that there must be one-to-one mapping between the letters in both strings
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) != len(t) : return False
+        if len(s) != len(t) : return False #This line is actually optional since the problem guarantees both strings are of equal length
         charMaps = dict()
         charMapt = dict() #Need two dictionaries to verify one-to-one mapping
         for i in range(len(s)):
@@ -18,6 +18,18 @@ class Solution:
                 return False
 
         return True #We only exit the loop and reach this line if they are isomorphic strings
+
+    #Alternate, very clever solution. Possibly less efficient though since using the index() method repeatedly is inefficient 
+    def isIsomorphic2(self, s: str, t: str) -> bool:
+        map1 = []
+        map2 = []
+        for letter in s:
+            map1.append(s.index(letter)) #Append the first index where letter occurs in string s
+        for letter in t:
+            map2.append(t.index(letter)) #Append the first index where letter occurs in string t
+        print(map1)
+        print(map2)
+        return map1 == map2
 
 def main():
     solution = Solution()
