326cb89b01223178cfb8a456f2550b8f8bbe20a7
Adam Rizk <adamrizk9@gmail.com>
2023-02-18 20:34:36 +0530

    Added remove elements from linked list problem

---
+++ DSA/removeLinkedListElements/solution.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+#https://leetcode.com/problems/remove-linked-list-elements/
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def printList(self) :
+        cur = self
+        numstr = ""
+        while cur :
+            numstr += str(cur.val) + " "
+            cur = cur.next
+        print(numstr)
+
+def createList(nums: list[int]) -> ListNode :
+    if len(nums) < 1 : return None
+    cur = ListNode(nums[0])
+    head = cur
+    for i in range(1,len(nums)):
+        cur.next = ListNode(nums[i])
+        cur = cur.next
+    return head
+
+class Solution:
+    #Using a dummy node works well for this problem since it alleviates us of the edge case of changing the head
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummyNode = ListNode(-1,head) #Add a dummy node before the head of the list
+        currNode = dummyNode
+        while currNode.next: #Keep iterating up until currNode is the second last Node in the list
+            if currNode.next.val == val: #Check the value of the node in front of currNode
+                currNode.next = currNode.next.next #Skip the unwanted node by setting the next pointer accordingly
+            else: #Important: We use an else statement since we do not want to increment currNode if we just removed the node ahead!
+                currNode = currNode.next
+        return dummyNode.next
+
+
+def main():
+    linkedList1 = createList([1,2,6,6,6,3,3,5,6,7,6])
+    solution = Solution()
+    newLinkedList1 = solution.removeElements(linkedList1, 6)
+    newLinkedList1.printList()
+    
+    
+
+if __name__ == "__main__": #Entry point
+    main() #Calling main method
