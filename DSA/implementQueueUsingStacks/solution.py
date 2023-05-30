1fd6932336c46fc18ff870e5a356995ca98eb0af
Adam Rizk <adamrizk9@gmail.com>
2023-05-30 19:37:25 +0530

    Notes

--- DSA/implementQueueUsingStacks/solution.py
+++ DSA/implementQueueUsingStacks/solution.py
@@ -39,7 +39,8 @@ class MyQueue:
 #More efficient approach. Instead of repeatedly emptying stack1 and stack2 each time a push or pop is called, we only do it when necessary
 #Obviously we have to empty stack1 into stack2 the first time pop or pull is called, but after that we hold on the values in stack2 for further peeks and pops.
 #When stack2 is empty after repeated pops, we then empty stack1 into stack2 again. We always insert into stack1 since we cannot insert into stack2 without messing up its order.
-#Since stack1 is supposed to have old elements to the left, and stack2 has old elements to the right
+#Since stack1 is supposed to have old elements to the left, and stack2 has old elements to the right.
+#The amortized complexity is O(1), even though the first pop or peek takes O(n)
 class MyQueue2:
 
     def __init__(self):
