1a400f52e4368cfad942de669a3396d3d524facb
Adam Rizk <adamrizk9@gmail.com>
2023-06-01 04:58:56 +0530

    Symmetric tree

--- DSA/symmetricTree/solution.py
+++ DSA/symmetricTree/solution.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from collections import deque
 
 #https://leetcode.com/problems/symmetric-tree/
 
@@ -27,8 +28,10 @@ def createList(nums: list[int]) -> ListNode :
 """
 
 class Solution:
-    #Somewhat similar to the same tree problem, but have to be more clever
+    #Somewhat similar to the same tree problem, but have to be more clever. Recursive solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+
+        #This helper method assumes that everything above leftRoot and rightRoot is symmetric
         def symmetricNodes(leftRoot, rightRoot): #This helper function is recursive
             if leftRoot == None and rightRoot == None : return True #Terminal Case
             if leftRoot == None or rightRoot == None : return False #Terminal Case
@@ -38,6 +41,28 @@ class Solution:
 
         if root : return symmetricNodes(root.left, root.right)
         else : return True
+
+    #Iterative approach
+    def isSymmetric2(self, root: Optional[TreeNode]) -> bool:
+        q = deque() #Use a queue since this is a type of BFS
+        q.append(root.left)
+        q.append(root.right)
+        while q:
+            l = q.popleft()
+            r = q.popleft()
+            #The front two nodes in the queue are the ones that need to be compared
+            if l == None and r == None: #Positive terminal case, can skip this iteration
+                continue
+            if l == None or r == None or l.val != r.val: #Negative terminal case, can return False immediately
+                return False
+            #The below lines are key. We add nodes in order such that two nodes that should be equal will be right next to each other in the queue.
+            #That way when we deque two nodes in the next iteration, those two nodes should be equal.
+            q.append(l.left)
+            q.append(r.right)
+            q.append(l.right)
+            q.append(r.left)
+        
+        return True
 
 
 def main():
