8bd2c086f61743509cf25d13510fa66ccb6fbe83
Adam Rizk <adamrizk9@gmail.com>
2023-06-14 23:39:22 +0530

    Notes

--- DSA/isPalindromeLinkedList/solution.py
+++ DSA/isPalindromeLinkedList/solution.py
@@ -50,13 +50,12 @@ class Solution:
             fastNode = fastNode.next.next
             temp = prevNode
             prevNode = slowNode #prevNode is now slowNode, since slowNode will move forward...
-            #print(f'{prevNode.val}')
             slowNode = slowNode.next #Increment slowNode
             prevNode.next = temp #Make prevNode point to the node behind it
             #Note that the ordering of the above pointer assignments is important. You can avoid this trouble of ordering by doing everything in one line,
             #such as prevNode, prevNode.next, slowNode = slowNode, prevNode, slowNode.next
             
-        #At the end of this while loop, fastNode is either the last node in the list (if there are an odd number of elements)
+        #At the end of the above while loop, fastNode is either the last node in the list (if there are an odd number of elements)
         #or, fastNode equals null if there are an even number of elements. This is because fastNode starts at node 1 (head) and keeps
         #jumping two steps. So it will go from node 1 to 3 to 5...
         #Meanwhile slowNode will either the exact middle if there an odd number of elements, or slowNode will be a bit right of the middle
