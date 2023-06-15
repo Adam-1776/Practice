0f7f0b59fe4e6a317c9c9bbeb66046eef9d812d0
Adam Rizk <adamrizk9@gmail.com>
2023-06-15 18:59:53 +0530

    max level sum of a binary tree

---
+++ DSA/maximumLevelSumBinaryTree/solution.py
@@ -0,0 +1,57 @@
+from collections import deque
+from math import inf
+from typing import Optional
+
+#https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+    def printList(self) :
+        cur = self
+        numstr = ""
+        while cur :
+            numstr += str(cur.val) + " "
+            cur = cur.next
+        print(numstr)
+def createList(nums: list[int]) -> ListNode :
+    if len(nums) < 1 : return None
+    cur = ListNode(nums[0])
+    head = cur
+    for i in range(1,len(nums)):
+        cur.next = ListNode(nums[i])
+        cur = cur.next
+    return head
+"""
+
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        level, levelSum = 1, float(-inf)
+        queue = deque()
+        currLevel = 1
+        if root:
+            queue.append(root)
+        while queue:
+            sum = 0
+            for _ in range(len(queue)):
+                currNode = queue.popleft()
+                sum += currNode.val
+                if currNode.left:
+                    queue.append(currNode.left)
+                if currNode.right:
+                    queue.append(currNode.right)
+            if sum > levelSum:
+                level, levelSum = currLevel, sum
+            currLevel += 1
+        return level
+
+def main():
+    print('No test case available')
+    
+    
+
+if __name__ == "__main__": #Entry point
+    main() #Calling main method
